2f0bd8e4e9d3dc9fa7e9739ee4d1c9c33fcca12a
MarkoM91 <markomedic91@gmail.com>
2019-04-22 19:26:19 +0200

    commit38

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function hiddenNavShow() {
 
+  var x = window.matchMedia("(max-width: 768px)");
+  var y = window.matchMedia("(min-width: 768px)");
   var hidden_header_right = $(".hidden-header-right");
-  hidden_header_right.toggleClass("active");
+
+  if (x.matches) {
+    hidden_header_right.addClass("active");
+  } else if (y.matches) {
+    hidden_header_right.removeClass("active");
+  }
 }
 
 function init() {
-  
+
   var menu = $("div.hamburger-menu");
   menu.click(hiddenNavShow);
 }
