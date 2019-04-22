e63230f58cc776c2b266e80c7950232d2629a810
MarkoM91 <markomedic91@gmail.com>
2019-04-22 18:45:11 +0200

    commit37

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function hiddenNavShow() {
 
   var hidden_header_right = $(".hidden-header-right");
   hidden_header_right.toggleClass("active");
-
 }
 
 function init() {
+  
   var menu = $("div.hamburger-menu");
   menu.click(hiddenNavShow);
 }
