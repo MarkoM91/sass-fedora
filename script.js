ac1940df0fd95c1a5ab9f63d7348cf3bcddb3b41
MarkoM91 <markomedic91@gmail.com>
2019-04-22 19:27:49 +0200

    commit39

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function hiddenNavShow() {
 
   var x = window.matchMedia("(max-width: 768px)");
-  var y = window.matchMedia("(min-width: 768px)");
   var hidden_header_right = $(".hidden-header-right");
 
   if (x.matches) {
     hidden_header_right.addClass("active");
-  } else if (y.matches) {
-    hidden_header_right.removeClass("active");
   }
 }
 
