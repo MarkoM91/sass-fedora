5490e73c80d0a3e0bd5a09756c9a141ba61a1905
MarkoM91 <markomedic91@gmail.com>
2019-04-23 15:32:41 +0200

    commit61

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-function hiddenNavShow() {
+function sliderImg() {
+  var activeSlider = $('div.slider.active');
+  activeSlider.removeClass("active");
 
+  var nextSlider;
+  if (activeSlider.hasClass("last")) {
+
+    nextSlider = $('div.slider.first');
+  } else {
+    nextSlider = activeSlider.next("div.slider");
+  }
+
+  nextSlider.addClass("active");
+}
+
+function hiddenNavShow() {
   var hidden_header_right = $(".hidden-header-right");
   hidden_header_right.toggleClass("active");
 }
 
 function resize() {
-
     var ww = document.body.clientWidth;
     if (ww >750) {
       $('.hidden-header-right').removeClass('active');
     }
-  }
+}
 
 function init() {
-
   var menu = $("div.hamburger-menu");
   menu.click(hiddenNavShow);
 
   $(window).resize(resize);
+
+  setInterval(sliderImg, 7000);
 }
 
 $(document).ready(init);
